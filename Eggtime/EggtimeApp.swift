33f77bdc8910f16c5caf58a5b4cd4d6e08b4a0cc
mfsaglam <mfsaglam1@gmail.com>
2021-09-24 14:16:54 +0300

    Initial Commit

---
+++ Eggtime/EggtimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggtimeApp.swift
+//  Eggtime
+//
+//  Created by Fatih Sağlam on 24.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct EggtimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
