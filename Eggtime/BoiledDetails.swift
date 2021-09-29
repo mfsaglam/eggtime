78c35a6c460589c855ecdca32f63facf7bd45b8d
mfsaglam <mfsaglam1@gmail.com>
2021-09-29 22:27:51 +0300

    Create more generic buttons

--- Eggtime/BoiledDetails.swift
+++ Eggtime/BoiledDetails.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct BoiledDetails: View {
+    
+    @State var temperature: String = "Room temperature"
+    @State var size: String = "M"
+    @State var boiledType = "Soft"
+    
     var body: some View {
         VStack(spacing: 20) {
             HStack(alignment: .top) {
@@ -32,9 +37,7 @@ struct BoiledDetails: View {
             VStack(spacing: 25) {
                 ThinBoldHeader(thinText: "Egg", boldText: "temperature")
                 HStack(spacing: 0) {
-                    ButtonStandart(text: "Fridge temperature", horizontalPadding: 20, isSelected: true)
-                    Spacer()
-                    ButtonStandart(text: "Room temperature", horizontalPadding: 20, isSelected: false)
+                    ButtonStandart(selectedItem: $temperature, text: ["Fridge temperature", "Room temperature"], horizontalPadding: 20)
                 }
                 
             }
@@ -42,22 +45,14 @@ struct BoiledDetails: View {
             VStack(spacing: 25) {
                 ThinBoldHeader(thinText: "Egg", boldText: "size")
                 HStack(spacing: 0) {
-                    ButtonStandart(text: "S", horizontalPadding: 45, isSelected: true)
-                    Spacer()
-                    ButtonStandart(text: "M", horizontalPadding: 45, isSelected: false)
-                    Spacer()
-                    ButtonStandart(text: "L", horizontalPadding: 45, isSelected: false)
+                    ButtonStandart(selectedItem: $size, text: ["S", "M", "L"], horizontalPadding: 45)
                 }
             }
             
             VStack(spacing: 50) {
                 ThinBoldHeader(thinText: "Egg", boldText: "boiled type")
                 HStack(spacing: 0) {
-                    ButtonWithEgg(text: "Soft", isSelected: true)
-                    Spacer()
-                    ButtonWithEgg(text: "Medium", isSelected: false)
-                    Spacer()
-                    ButtonWithEgg(text: "Hard", isSelected: false)
+                    ButtonWithEgg(text: ["Soft", "Medium", "Hard"], selectedItem: $boiledType)
                 }
             }
             
@@ -90,59 +85,83 @@ struct BoiledDetails: View {
 struct BoiledDetails_Previews: PreviewProvider {
     static var previews: some View {
         BoiledDetails()
-            .preferredColorScheme(.dark)
+            
     }
 }
 
 struct ButtonWithEgg: View {
-    var text: String
-    var isSelected: Bool
+    var text: [String]
+    @Binding var selectedItem: String
     var body: some View {
-        ZStack {
-            VStack(alignment: .leading) {
-                Text(text)
-                Text("boiled").bold()
+        ForEach(text.indices) { tag in
+            ZStack {
+                VStack(alignment: .leading) {
+                    Text(text[tag])
+                    Text("boiled").bold()
+                }
+                .foregroundColor(text[tag] == selectedItem ? Color(#colorLiteral(red: 0.9013573527, green: 0.5734035373, blue: 0.4102450013, alpha: 1)) : .primary)
+                .padding(.vertical)
+                .frame(width: 100, height: 120, alignment: .bottom)
+                .background(RoundedRectangle(cornerRadius: 5)
+                                .stroke(lineWidth: 2)
+                )
+                .foregroundColor(text[tag] == selectedItem ? Color(#colorLiteral(red: 0.9013573527, green: 0.5734035373, blue: 0.4102450013, alpha: 1)) : .secondary)
+                CookedEgg()
+                    .scaleEffect(x: 0.25, y: 0.25, anchor: .center)
+                    .frame(width: 20, height: 20)
+                    .offset(y: -50)
             }
-            .foregroundColor(isSelected ? Color(#colorLiteral(red: 0.9013573527, green: 0.5734035373, blue: 0.4102450013, alpha: 1)) : .primary)
-            .padding(.vertical)
-            .frame(width: 100, height: 120, alignment: .bottom)
-            .background(RoundedRectangle(cornerRadius: 5)
-                            .stroke(lineWidth: 2)
-            )
-            .foregroundColor(isSelected ? Color(#colorLiteral(red: 0.9013573527, green: 0.5734035373, blue: 0.4102450013, alpha: 1)) : Color(#colorLiteral(red: 0.8038417101, green: 0.8039775491, blue: 0.8038237691, alpha: 1)))
-            CookedEgg()
-                .scaleEffect(x: 0.25, y: 0.25, anchor: .center)
-                .frame(width: 20, height: 20)
-                .offset(y: -50)
+            .onTapGesture {
+                withAnimation(.interactiveSpring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.6)) {
+                    selectedItem = text[tag]
+                }
+            }
+            if tag != text.endIndex - 1 {
+                Spacer()
+            }
         }
     }
 }
 
 struct ButtonStandart: View {
-    var text: String
+    
+    @Binding var selectedItem: String
+    var text: [String]
     var horizontalPadding: CGFloat
-    var isSelected: Bool
+    
     var body: some View {
-        HStack {
-            if text.contains(" ") {
-                HStack {
-                    Text("\(text.components(separatedBy: " ")[0])")
-                        .font(.callout)
-                    Text("\(text.components(separatedBy: " ")[1])")
-                        .font(.callout)
-                        .bold()
+        ForEach(text.indices) { tag in
+            HStack {
+                Group {
+                    if text[tag].contains(" ") {
+                        HStack {
+                            Text("\(text[tag].components(separatedBy: " ")[0])")
+                                .font(.callout)
+                            Text("\(text[tag].components(separatedBy: " ")[1])")
+                                .font(.callout)
+                                .bold()
+                        }
+                    } else {
+                        Text(text[tag])
+                            .font(.headline)
+                    }
                 }
-            } else {
-                Text(text)
-                    .font(.headline)
+                .padding(.horizontal, horizontalPadding)
+                .padding(.vertical, 19)
+                .background(RoundedRectangle(cornerRadius: 5)
+                                .stroke(lineWidth: 2)
+                )
+                .foregroundColor(selectedItem == text[tag] ? Color(#colorLiteral(red: 0.9013573527, green: 0.5734035373, blue: 0.4102450013, alpha: 1)) : Color(#colorLiteral(red: 0.8038417101, green: 0.8039775491, blue: 0.8038237691, alpha: 1)))
+                .onTapGesture {
+                    withAnimation(.interactiveSpring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.6)) {
+                        selectedItem = text[tag]
+                    }
+                }
+                if tag != text.endIndex - 1 {
+                    Spacer()
+                }
             }
         }
-        .padding(.horizontal, horizontalPadding)
-        .padding(.vertical, 19)
-        .background(RoundedRectangle(cornerRadius: 5)
-                        .stroke(lineWidth: 2)
-        )
-        .foregroundColor(isSelected ? Color(#colorLiteral(red: 0.9013573527, green: 0.5734035373, blue: 0.4102450013, alpha: 1)) : Color(#colorLiteral(red: 0.8038417101, green: 0.8039775491, blue: 0.8038237691, alpha: 1)))
     }
 }
 
@@ -158,4 +177,8 @@ struct ThinBoldHeader: View {
             Spacer()
         }
     }
+}
+
+enum Temperature {
+    case fridge, room
 }
